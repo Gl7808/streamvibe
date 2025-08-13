bdc2e7331564affeddacc16c5335af01155e18d7
Gl7808 <whorshunter42@yandex.ru>
2025-08-13 16:51:08 +0300

    2.4

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,16 @@
+import './Logo.scss'
+import classNames from "classnames";
+const Logo = (props) => {
+    const {
+        className,
+        loading = 'lazy',
+    } = props
+    const title = 'Home'
+    return(
+        <a className={classNames(className, 'logo')} href='/' title={title} aria-label={title}>
+            <img src="/Logo.svg" alt="" className="logo__image" alt="" width="199" height="60" loading={loading}/>
+        </a>
+    )
+}
+
+export default Logo
